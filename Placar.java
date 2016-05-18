cf555a0012ada21d69def42fb9290237d3d05259
DanielHara <haradaniel734@gmail.com>
2016-05-18 10:15:26 -0300

    Update Placar.java

--- Placar.java
+++ Placar.java
@@ -1,6 +1,7 @@
 import javax.swing.*;     
 import java.awt.*; 
 
+Esta classe controla o Placar.
 
 public class Placar extends JPanel{
 
@@ -8,7 +9,7 @@ public class Placar extends JPanel{
 	private int n_Propina;
 	private int n_Prisoes;
 	
-	
+	//Construtor
 	public Placar ()
 	{
 		textArea.setEditable(false);
@@ -18,29 +19,34 @@ public class Placar extends JPanel{
 		Show_Placar();
 	}
 	
+	//Esta função mostra o placar.
 	private void Show_Placar ()
 	{
 		textArea.setText("");
 		textArea.append("Propina: R$" + n_Propina + "Bilhões \nPrisões: " + n_Prisoes);
 	}
 	
+	//Esta função acrescenta 1 prisão
 	public void Acrescentar_Prisao ()
 	{
 		n_Prisoes++;
 		Show_Placar();
 	}
 	
+	//Esta função acrescenta 1 propina
 	public void Acrescentar_Propina ()
 	{
 		n_Propina++;
 		Show_Placar();
 	}
 	
+	//Esta função retorna o número de prisões.
 	public int get_n_Prisoes ()
 	{
 		return n_Prisoes;
 	}
 	
+	//Esta função retorna o número de propinas.
 	public int get_n_Propina ()
 	{
 		return n_Propina;
